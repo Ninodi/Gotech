695ff2e7f8291fe4d9bdeacf2e54436c1e17fd3e
Ninodi <78281735+Ninodi@users.noreply.github.com>
2021-05-06 23:48:45 +0400

    .

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,18 +8,3 @@ scrollBtn.addEventListener('click', () => {
 })
 
 
-
-let person = [10, 5, 'mobile', [20, 'car']]
-
-
-for(var i = 0; i < person.length; i++){
-    if(typeof(person[i]) === 'object') {
-        let my = new Array(person[i])
-        // my.toString()
-        array.from(Array)
-        for(k = 0; k < my.length; k++){
-            console.log(my[k])
-        }
-    } else
-    console.log(person[i])
-}
